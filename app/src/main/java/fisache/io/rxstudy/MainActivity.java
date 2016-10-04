abf99d6af8c32923a5d016e42106f952eccaa36d
fisache <hwang031451@gamil.com>
2016-10-04 17:22:50 +0900

    Modified mainactivity

--- app/src/main/java/fisache/io/rxstudy/MainActivity.java
+++ app/src/main/java/fisache/io/rxstudy/MainActivity.java
@@ -25,57 +25,70 @@ public class MainActivity extends AppCompatActivity {
         Button btn = (Button) findViewById(R.id.btn);
         btn.setOnClickListener(v ->{
             etResult.setText("");
-            // 람다 코드 구현
+        // 람다 코드 구현
+        Observable.just(etEdit.getText().toString())
+                .map(dan -> Integer.parseInt(dan))
+                .filter(dan -> 1 < dan && dan < 10)
+                .flatMap(dan -> Observable.range(1, 9),
+                        (dan, row) -> dan + " * " + row + " = " + (dan*row))
+                .map(row -> row+"\n")
+                .subscribe(
+                        etResult::append,
+                        e -> Toast.makeText(getBaseContext(), "Not between 2 and 9", Toast.LENGTH_LONG).show()
+                );
+//            // 람다 사용 안한 코드 구현
 //            Observable.just(etEdit.getText().toString())
-//                    .map(dan -> Integer.parseInt(dan))
-//                    .filter(dan -> 1 < dan && dan < 10)
-//                    .flatMap(dan -> Observable.range(1, 9),
-//                            (dan, row) -> dan + " * " + row + " = " + (dan*row))
-//                    .map(row -> row+"\n")
+//                    .map(new Func1<String, Integer>() {
+//                        @Override
+//                        public Integer call(String dan) {
+//                            return Integer.parseInt(dan);
+//                        }
+//                    })
+//                    .filter(new Func1<Integer, Boolean>() {
+//                        @Override
+//                        public Boolean call(Integer dan) {
+//                            if(dan > 1 && dan < 10) {
+//                                return true;
+//                            }
+//                            return false;
+//                        }
+//                    })
+//                    .flatMap(new Func1<Integer, Observable<String>>() {
+//                        @Override
+//                        public Observable<String> call(Integer dan) {
+//                            return Observable.range(1,9)
+//                                    .map(new Func1<Integer, String>() {
+//                                        @Override
+//                                        public String call(Integer row) {
+//                                            return dan + " * " + row + " = " + (dan*row);
+//                                        }
+//                                    });
+//                        }
+//                    })
+//                    .map(new Func1<String, String>() {
+//                        @Override
+//                        public String call(String row) {
+//                            return row+"\n";
+//                        }
+//                    })
 //                    .subscribe(
-//                            etResult::append,
-//                            e -> Toast.makeText(getBaseContext(), "Not between 2 and 9", Toast.LENGTH_LONG).show()
+//                            new Observer<String>() {
+//                                @Override
+//                                public void onCompleted() {
+//                                    Toast.makeText(getBaseContext(), "onCompleted", Toast.LENGTH_LONG).show();
+//                                }
+//
+//                                @Override
+//                                public void onError(Throwable e) {
+//                                    Toast.makeText(getBaseContext(), "Not between 2 and 9", Toast.LENGTH_LONG).show();
+//                                }
+//
+//                                @Override
+//                                public void onNext(String row) {
+//                                    etResult.append(row);
+//                                }
+//                            }
 //                    );
-            // 람다 사용 안한 코드 구현
-            Observable.just(etEdit.getText().toString())
-                    .map(dan -> Integer.parseInt(dan))
-                    .filter(dan -> 1 < dan && dan < 10)
-                    .flatMap(new Func1<Integer, Observable<String>>() {
-                        @Override
-                        public Observable<String> call(Integer dan) {
-                            return Observable.range(1,9)
-                                    .map(new Func1<Integer, String>() {
-                                        @Override
-                                        public String call(Integer row) {
-                                            return dan + " * " + row + " = " + (dan*row);
-                                        }
-                                    });
-                        }
-                    })
-                    .map(new Func1<String, String>() {
-                        @Override
-                        public String call(String integer) {
-                            return integer+"\n";
-                        }
-                    })
-                    .subscribe(
-                            new Observer<String>() {
-                                @Override
-                                public void onCompleted() {
-                                    Toast.makeText(getBaseContext(), "onCompleted", Toast.LENGTH_LONG).show();
-                                }
-
-                                @Override
-                                public void onError(Throwable e) {
-                                    Toast.makeText(getBaseContext(), "Not between 2 and 9", Toast.LENGTH_LONG).show();
-                                }
-
-                                @Override
-                                public void onNext(String s) {
-                                    etResult.append(s);
-                                }
-                            }
-                    );
         });
 
     }
